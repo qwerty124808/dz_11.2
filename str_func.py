4ade3211ebc6cbc1bc04be5119e90ec66e39da0b
altarev_da <altarev_da@nlmk.com>
2023-03-16 20:22:49 +0300

    add all in dev_2

--- str_func.py
+++ str_func.py
@@ -1,3 +1,11 @@
 def string(lov_text):
+    """
+    преобразует все символы строки в верх. регистр
+    """
     return lov_text.upper()
 
+def string_2(lov_text):
+    """
+    преобразует первый символ строки в верх. регистр
+    """
+    return lov_text.capitalize()
